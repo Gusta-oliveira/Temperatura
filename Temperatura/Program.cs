765084e27abf0d12e75678ded04c19053ac1c522
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-06 00:43:18 -0300

    Add project files.

---
+++ Temperatura/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double celsius, fah;
+
+        Console.WriteLine("Informe a temperatura em ºC: ");
+        celsius = float.Parse(Console.ReadLine());
+        fah = ((celsius * 9)/5) + 32;
+        Console.WriteLine("A temperatura {0}ºC convertidaem {1}ºF", celsius, fah);
+    }
+}
